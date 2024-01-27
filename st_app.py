c28f05e4e52251915a2e8fc70546cb95d07dfeb6
Jay Sepehri <sepehri.jay@gmail.com>
2024-01-26 21:23:45 -0600

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -1,9 +1,17 @@
 import streamlit as st
+import yfinance as yf
+import pandas
 
 st.write(""" 
-# Hello World!
-This is my **first** *app*!
-I'm just testing
-adding new lines
+# simple Stock Price App
 
+Apple Closing Stock Price and Volumes
 """)
+
+tickerSymbol ="AAPL"
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period='1d', start = '2023-5-31', end='2024-1-20')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
